4ce42131e131dc4d7ce9d85accc9fde6c1346dd2
KevinB-a <kevin.billet.pro@gmail.com>
2020-10-14 10:56:00 +0200

    add for and while loops

---
+++ loops_and_condition.js
@@ -0,0 +1,24 @@
+/* la boucle for doit etre codée de cette facon
+    for ([expressionInitiale]; [condition]; [expressionIncrément]) 
+    {
+        instruction 
+    }
+  
+*/
+
+// exemple de boucle for
+for (let pas = 0; pas < 5; pas++) 
+{
+    // Ceci sera exécuté 5 fois
+    // À chaque éxécution, la variable "pas" augmentera de 1
+    // Lorsque'elle sera arrivée à 5, le boucle se terminera.
+    console.log('Faire ' + pas + ' pas vers l\'est'); // l'anti slash sert a placer l'apostrophe
+}
+
+/* la boucle while doit etre codee de cette facon 
+    while (condition)
+    {
+        instruction 
+    }
+        
+*/
